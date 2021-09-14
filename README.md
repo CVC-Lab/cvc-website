bfb16cbce64395e90cfcebe7b154f38516163d0e
Sean Zanyk McLean <seanzanykmclean@wireless-10-145-107-239.public.utexas.edu>
2021-09-14 12:18:19 -0500

    first commit, starting with gatsby-starter repo

---
+++ README.md
@@ -0,0 +1,2 @@
+## Computation Visualization Center Website
+This repo will be the src for the new CVC website.
---
+++ src/components/header.js
@@ -0,0 +1,61 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+
+const Header = ({ siteTitle, menuLinks }) => (
+  <header
+    style={{
+      background: `#bf5700`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <div>
+        <nav>
+          <ul style={{ display: "flex", flex: 1}}>
+            {menuLinks.map(link => (
+              <li
+                key={link.name}
+                style={{
+                  listStyleType: `none`,
+                  padding: `1rem`,
+                }}
+              >
+                <Link style={{ color: `white` }} to={link.link}>
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
