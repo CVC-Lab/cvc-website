c1b772b84a07f5168960533c4c8e12f3276e0c1f
Sean Zanyk McLean <seanzanykmclean@wireless-10-145-56-32.public.utexas.edu>
2021-10-15 12:58:47 -0500

    fix nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,13 +1,20 @@
 import * as React from "react"
 import { Link } from "react-scroll"
 import { Link as LinkRouter } from "gatsby";
+import { useLocation } from '@reach/router';
+
+const UserTheme = () => {
+  const location = useLocation();
+  return location.pathname
+};
+
 const Navigation = ({ menuLinks }) => (
   <div style={{display: 'flex', justifyContent:'flex-end'}}>
     <nav>
       <ul style={{display: "flex", flex: 1}}>
         {menuLinks.map(link => (
           <li key={link.name} style={{listStyleType: `none`, padding: `1rem`, paddingTop: `.35rem`, paddingBottom: `0rem`}}>
-              {link.scroll_link == true && typeof window !== `undefined` && window.location.pathname === `/` ?
+              {link.scroll_link === true && (UserTheme() === `/cvc-website/` || UserTheme() === `/`) ?
                 <LinkRouter to={link.link} style={{color: `#333f48`, textDecoration: `none`, fontSize: `.73rem`, fontWeight: `700`}}>
                 <Link
                   style={{color: `#333f48`, textDecoration: `none`, fontSize: `.73rem`, fontWeight: `700`}}
@@ -20,7 +27,7 @@ const Navigation = ({ menuLinks }) => (
                 >
                 {link.name}
                 </Link></LinkRouter>
-                :<LinkRouter to={link.scroll_link == true ? '/': link.link} style={{color: `#333f48`, textDecoration: `none`, fontSize: `.73rem`, fontWeight: `700`}}>
+                :<LinkRouter to={link.scroll_link === true ? '/': link.link} style={{color: `#333f48`, textDecoration: `none`, fontSize: `.73rem`, fontWeight: `700`}}>
                   {link.name}
                 </LinkRouter>}
           </li>))}
