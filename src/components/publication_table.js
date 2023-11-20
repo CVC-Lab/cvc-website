a444bd3670b9042168eb85209f3d034ab6ec6628
ava <ava@users.noreply.github.com>
2023-11-20 11:02:46 -0600

    Group publications by year

--- src/components/publication_table.js
+++ src/components/publication_table.js
@@ -2,63 +2,56 @@ import * as React from "react";
 import { Grid } from "@mui/material";
 import "./publication_table.css";
 
-const PublicationTable = ({ publicationData = [] }) => (
-  <div
-    className="publications-class"
-    id="publications"
-    style={{
-      background: "white",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      textAlign: `left`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 1050,
-        paddingBottom: `1.45rem`,
-        marginLeft: `1.25rem`,
-        marginRight: `1.25rem`,
-        width: `100%`,
-      }}
-    >
-      <h4
-        className="header-subtitle"
-        style={{
-          color: `#787575`,
-          fontSize: `1.0rem`,
-          fontWeight: `600`,
-          margin: `auto`,
-          paddingBottom: `1.0rem`,
-          paddingTop: `2.0rem`,
-          marginBottom: "1rem",
-        }}
-      >
-        Publications
-      </h4>
-      <Grid container direction="column" spacing={2}>
-        {publicationData
-          .sort((a, b) => b.PublishedDateYear - a.PublishedDateYear)
-          .map((publication) => (
-            <Grid item key={publication.Authors}>
-              <div className="Card">
-                <div className="lower-container-pubs">
-                  <h3> {publication.Title} </h3>
-                  <h4> {publication.Authors} </h4>
-                  <h4>
-                    {publication.PublishedDateYear} (
-                    {publication.PublicationType} -{" "}
-                    <a href={publication.PDFLink}>pdf</a>)
-                  </h4>
-                </div>
-              </div>
-            </Grid>
+const groupByYear = (publications) => {
+  return publications.reduce((groupedPublications, publication) => {
+    const year = publication.PublishedDateYear;
+    if (!groupedPublications[year]) {
+      groupedPublications[year] = [];
+    }
+    groupedPublications[year].push(publication);
+    return groupedPublications;
+  }, {});
+};
+
+const PublicationTable = ({ publicationData = [] }) => {
+  const groupedPublications = groupByYear(publicationData);
+  
+  return (
+    <div className="publications-class" id="publications">
+      <div className="publication-container">
+        <h4 className="header-subtitle">Publications</h4>
+        <Grid container direction="column" spacing={2}>
+          {Object.entries(groupedPublications)
+            .sort(([year1], [year2]) => year2 - year1)
+            .map(([year, publications]) => (
+            <div key={year} style={{marginBottom: "30px"}}>
+              <Grid item className="year-header">
+                <h3>{year}</h3>
+              </Grid>
+              {publications.map((publication) => (
+                <Grid item key={publication.Authors} style={{marginBottom: "15px"}}> 
+                  <div className="Card">
+                    <div className="lower-container-pubs">
+                      <h3> {publication.Title} </h3>
+                      <h4> {publication.Authors} </h4>
+                      <h4>
+                      {publication.PublishedDateYear} (
+                        {publication.PublicationType}
+                        {publication.PDFLink && publication.PDFLink !== 'NULL' && (
+                        <> - <a href={publication.PDFLink}>pdf</a></>
+                        )}
+                      )
+                      </h4>
+                    </div>
+                  </div>
+                </Grid>
+              ))}
+            </div>
           ))}
-      </Grid>
+        </Grid>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default PublicationTable;
